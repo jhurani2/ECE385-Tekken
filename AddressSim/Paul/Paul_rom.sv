76d446b87c5af92973f06bb7c5b25e8258d26793
jhurani2 <jhurani2@illinois.edu>
2026-04-21 22:27:20 -0500

    COE files and assets

---
+++ AddressSim/Paul/Paul_rom.sv
@@ -0,0 +1,13 @@
+module Paul_rom (
+	input logic clock,
+	input logic [12:0] address,
+	output logic [2:0] q
+);
+
+logic [2:0] memory [0:8191] /* synthesis ram_init_file = "./Paul/Paul.COE" */;
+
+always_ff @ (posedge clock) begin
+	q <= memory[address];
+end
+
+endmodule
